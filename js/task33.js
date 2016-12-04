fb857f6600632254a9f37392645561516bc3d2c9
Minxine-Yao <seuhuangziyao@outlook.com>
2016-12-04 22:48:31 +0800

    动画效果还有问题，其它暂时无问题

--- js/task33.js
+++ js/task33.js
@@ -56,7 +56,7 @@ function createGrid(){
 					grid.style.borderBottom = "2px solid black";
 				grid.id = englishNum[i]+"-"+englishNum[j];
 				grids.appendChild(grid);				
-direction}
+			}
 		}
 	}
 }
@@ -68,27 +68,33 @@ function Square(id,x,y){
 	this.y = y;
 	this.lastHead = 0;
 	this.head = 0; //0代表目前头朝上
-	this.render = function(){
+	this.repaint = function(){
 		// 清除上一步
 		var lastDiv = document.getElementById(`${englishNum[this.lastY]}-${englishNum[this.lastX]}`);
-		lastDiv.style.backgroundColor = "white";
-		lastDiv.style[`border${direction[this.lastHead]}`] = "none";
-		lastDiv.style.borderRight = "1px solid rgb(200,200,200)";
-		lastDiv.style.borderBottom = "1px solid rgb(200,200,200)";
-		if(this.lastX===1) //最左侧的格子
-			lastDiv.style.borderLeft = "2px solid black";
-		if(this.lastY===1) //最上侧的格子
-			lastDiv.style.borderTop = "2px solid black";
-		if(this.lastX===gridSize) //最右侧的格子
-			lastDiv.style.borderRight = "2px solid black";
-		if(this.lastY===gridSize) //最下侧的格子
-			lastDiv.style.borderBottom = "2px solid black";
-		// 绘制下一步
-		var div = document.getElementById(`${englishNum[this.y]}-${englishNum[this.x]}`);
-		div.style.backgroundColor = "red";
-		div.style[`border${direction[this.head]}`] = "10px solid blue";
+		lastDiv.className += "grid active";
+		lastDiv.style.transform = `translate(${(this.x-this.lastX)*gridWidth}px,${(this.y-this.lastY)*gridWidth}px)`;
+		setTimeout(function(){
+			lastDiv.className = "grid";
+			lastDiv.style.transform = "none";
+			lastDiv.style.backgroundColor = "white";
+			lastDiv.style[`border${direction[this.lastHead]}`] = "none";
+			lastDiv.style.borderRight = "1px solid rgb(200,200,200)";
+			lastDiv.style.borderBottom = "1px solid rgb(200,200,200)";
+			if(this.lastX===1) //最左侧的格子
+				lastDiv.style.borderLeft = "2px solid black";
+			if(this.lastY===1) //最上侧的格子
+				lastDiv.style.borderTop = "2px solid black";
+			if(this.lastX===gridSize) //最右侧的格子
+				lastDiv.style.borderRight = "2px solid black";
+			if(this.lastY===gridSize) //最下侧的格子
+				lastDiv.style.borderBottom = "2px solid black";
+			// 绘制下一步
+			var div = document.getElementById(`${englishNum[this.y]}-${englishNum[this.x]}`);
+			div.style.backgroundColor = "red";
+			div.style[`border${direction[this.head]}`] = "10px solid blue";
+		},1000);
 	};
-	this.render();
+	this.repaint();
 	this.moveForward = function(distance){
 		var destX = this.x;
 		var destY = this.y;
@@ -101,26 +107,61 @@ function Square(id,x,y){
 			this.lastY = this.y;
 			this.x = destX;
 			this.y = destY;
-			this.render();
+			this.repaint();
 		}else{
 			alert("将移动出界！");
 		}
 	};
+	this.move = function(direction){
+		// 先转方向再平移
+		var turnTo;
+		this.lastHead = this.head;
+		if(direction==="TOP") turnTo = 0; //转向上方
+		if(direction==="RIG") turnTo = 1; //转向右方
+		if(direction==="BOT") turnTo = 2; //转向下方
+		if(direction==="LEF") turnTo = 3; //转向左方
+		this.head = turnTo;
+		this.moveForward(1);
+		this.repaint();
+	};
+	this.translate = function(direction){
+		// 平移
+		direction = direction.toUpperCase().trim();
+		var destX = this.x;
+		var destY = this.y;
+		if(direction==="TOP") destY-=1; //向上移动
+		if(direction==="RIG") destX+=1; //向右
+		if(direction==="BOT") destY+=1; //向下
+		if(direction==="LEF") destX-=1; //向左
+		if(destX <= gridSize && destX >= 1 && destY <= gridSize && destY >= 1 ){
+			this.lastX = this.x;
+			this.lastY = this.y;
+			this.x = destX;
+			this.y = destY;
+			this.repaint();
+		}else{
+			alert("将移动出界！");
+		}
+	}
 	this.rotate = function(rotateType){
 		this.lastHead = this.head;
 		rotateType = rotateType.toUpperCase().trim();
 		if(rotateType === "RIG") this.head = (this.head+1)%4;
 		if(rotateType === "LEF") this.head = (this.head-1 < 0) ? (this.head + 3) : (this.head - 1);
 		if(rotateType === "BAC") this.head = (this.head+2)%4;
-		this.render();
+		this.repaint();
 	};
 	this.istTranslate = function(ist){ //ist = instruction
 		ist = ist.toUpperCase().trim();
+		var opType = ist.slice(0,3);
+		var opNum = ist.slice(-3); 
 		if(ist === "GO") this.moveForward(1);
-		else if(ist.slice(0,3) === "TUN") this.rotate(ist.slice(-3));
+		else if(opType === "TUN") this.rotate(opNum);
+		else if(opType === "TRA") this.translate(opNum);
+		else if(opType === "MOV") this.move(opNum);
 		else alert("Wrong instruction!");
 
-	}
+	};
 }
 window.onload = function(){
 	createGrid();
